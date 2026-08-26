794f1c3f00983d2c396215dc80ed5d018ac395a7
2411062344-cell <2411062344@hunre.edu.vn>
2026-08-26 11:15:04 +0700

    Add files via upload

---
+++ buoi2bai1bai2.py
@@ -0,0 +1,11 @@
+ho_ten = input("nhap ho ten:")
+nam_sinh = int(input("nhap nam sinh:"))
+diem_tb = float(input("nhap diem trung binh:"))
+print("ho ten:", ho_ten)
+print("nam sinh:", nam_sinh)
+print("diem trung binh:", diem_tb)
+
+
+print("python", "la", "mot", "ngon", "ngu", "lap", "trinh", sep="-")
+print("dong 1", end="|")
+print("dong 2")
